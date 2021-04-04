7116100e7dc0b77adba905aafaed4a39f14c2fbe
Subhendu Dash <71781104+subhendudash02@users.noreply.github.com>
2021-04-04 19:23:43 +0530

    Create Miscommunication_from_Mars.c

---
+++ Miscommunication_from_Mars.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int n, d, re;
+
+    scanf(" %d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        scanf(" %d", &arr[i]);
+    }
+
+    scanf(" %d %d", &d, &re);
+
+    int fi[n];
+
+    for (int i = 0; i < n; i++){
+        for (int j = i; j < n; j++){
+            int x = arr[i] - arr[j];
+            if ((arr[i] != arr[j]) && (abs(x) == d) && ((arr[i] ^ arr[j]) == re)){
+                if (arr[i] < arr[j]){
+                    printf("%d \t %d\n", arr[i], arr[j]);
+                }
+                else{
+                    printf("%d \t %d\n", arr[j], arr[i]);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
